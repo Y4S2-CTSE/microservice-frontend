bea61c5c5f1a5abb48c28dbe7ca61f3d65c1e3ae
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2025-03-21 12:12:09 +0530

    check(netlify): Auto deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edited to check netlify auto deployment
         </p>
         <a
           className="App-link"
